c1de6ab0e904de4874b94187cc9e70e17ded159a
Gazi Hatas <gazihatas71@gmail.com>
2022-11-19 07:10:47 +0300

    Middleware uygulandi

--- routes/web.php
+++ routes/web.php
@@ -16,18 +16,26 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', function () {
-    return view('admin.index');
-});
 
-Auth::routes();
+
+Auth::routes([
+    'register'=>false,
+    'reset'=>false,
+    'verify'=>false
+]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('quiz',QuizController::class);
-Route::resource('question',QuestionController::class);
-Route::resource('user',UserController::class);
+Route::group(['middleware'=>'isAdmin'],function (){
+    Route::get('/', function () {
+        return view('admin.index');
+    });
+    Route::resource('quiz',QuizController::class);
+    Route::resource('question',QuestionController::class);
+    Route::resource('user',UserController::class);
 //Route::get('/quiz/{id}/question',[QuizController::class, 'question'])->name('quiz.question');
-Route::get(
-    '/quiz/{id}/question',
-    [QuizController::class, 'question']
-)->name('quiz.question');
+    Route::get(
+        '/quiz/{id}/question',
+        [QuizController::class, 'question']
+    )->name('quiz.question');
+});
+
