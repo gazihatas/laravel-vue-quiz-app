464a95236137c157ae8bc07a0dd99e430ce77dff
Gazi Hatas <gazihatas71@gmail.com>
2022-11-19 06:24:00 +0300

    seeder olusturuldu

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -20,5 +20,18 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $admin = new User();
+        $admin->name="admin";
+        $admin->email="admin@gmail.com";
+        $admin->password=bcrypt('password');
+        $admin->visible_password="password";
+        $admin->email_verified_at = NOW();
+        $admin->occupation="CEO";
+        $admin->address="Turkiye";
+        $admin->phone="05555555555";
+        $admin->is_admin=1;
+        $admin->save();
+
     }
 }
